ac79c061a1f9c0388d4bf38444cf70125dbb3645
Bhumika Basnet <119913690+Bhumika7@users.noreply.github.com>
2025-01-27 01:49:50 +0000

    Script file created

--- Hwork1/Q3and4.lisp
+++ Hwork1/Q3and4.lisp
@@ -1,13 +1,23 @@
 ;Author: Bhumika Basnet
-(defparameter *list1* '(a b x d))
-(defparameter *list2* '(a (b (x d))))
-(defparameter *list3* '((( a (b (x) d)))))
-(print (caddr *list1*))  
-(print (car(car (cdr (car (cdr *list2*))))))     
-(print (car(car(cdr(car(cdr(car(car *list3*))))))))  
+;CSCI 330
+;01/26/2025
+
+;; Define test lists
+(defparameter *list1* '(a b x d))         ; Flat list
+(defparameter *list2* '(a (b (x d))))     ; Nested list
+(defparameter *list3* '(((a (b (x) d))))) ; Deeply nested list
+
+;; Question 3: Access and print 'x' from each list
+(print (caddr *list1*))  ; Get 3rd element from flat list *list1*
+(print (car (car (cdr (car (cdr *list2*))))))  ; Get 'x' from *list2*
+(print (car (car (cdr (car (cdr (car (car *list3*))))))))  ; Get 'x' from *list3*
+
+;; Question 4: Recreate the lists using `cons`
 (defparameter *list1-cons* (cons 'a (cons 'b (cons 'x (cons 'd nil)))))
-(print *list1-cons*)  
+(print *list1-cons*)  ; should print (a b x d)
+
 (defparameter *list2-cons* (cons 'a (cons (cons 'b (cons (cons 'x (cons 'd nil)) nil)) nil)))
-(print *list2-cons*) 
+(print *list2-cons*)  ; should print (a (b (x d)))
+
 (defparameter *list3-cons* (cons (cons (cons 'a (cons (cons 'b (cons (cons 'x nil) (cons 'd nil))) nil)) nil) nil))
-(print *list3-cons*)  
+(print *list3-cons*)  ; should print ((( a (b (x) d))))
